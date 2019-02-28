828791dfe350333a47006a66ffe9ba93d856f297
Michelle Yu <y.michelle03@gmail.com>
2019-02-27 22:05:29 -0500

    first commit

---
+++ tumblr/PhotoCell.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoCell.swift
+//  tumblr
+//
+//  Created by ellehcim on 2/22/19.
+//  Copyright © 2019 ellehcim. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage;
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoCellImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
---
+++ tumblr/PhotosViewController.swift
@@ -0,0 +1,84 @@
+//
+//  PhotosViewController.swift
+//  tumblr
+//
+//  Created by ellehcim on 2/22/19.
+//  Copyright © 2019 ellehcim. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage;
+
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
+    @IBOutlet weak var postsTableView: UITableView!;
+    
+    // property 'posts' store posts
+    var posts: [[String: Any]] = [];
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+
+                print(dataDictionary);
+
+            }
+        }
+        
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell;
+        let post = posts[indexPath.row];
+        
+        if let photos = post["photos"] as? [[String: Any]] {
+            let photo = photos[0]
+            let originalSize = photo["original_size"] as! [String: Any]
+            let urlString = originalSize["url"] as! String
+            let url = URL(string: urlString)
+            
+            cell.photoCellImageView.af_setImage(withURL: url!);
+            
+        }
+        
+        
+        return cell;
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
